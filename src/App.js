50473251e5334eb16b07230e3ce6e1deb8bf4031
sgad-andlauers <71265055+sgad-andlauers@users.noreply.github.com>
2021-03-08 12:01:00 +0100

    add some feature

--- src/App.js
+++ src/App.js
@@ -2,21 +2,40 @@ import React, { useState, useEffect, forwardRef } from "react";
 import "./styles.css";
 import axios from "axios";
 import MaterialTable from "material-table";
-import AddBox from "@material-ui/icons/AddBox";
-import ArrowDownward from "@material-ui/icons/ArrowDownward";
-import Check from "@material-ui/icons/Check";
-import ChevronLeft from "@material-ui/icons/ChevronLeft";
-import ChevronRight from "@material-ui/icons/ChevronRight";
-import Clear from "@material-ui/icons/Clear";
-import DeleteOutline from "@material-ui/icons/DeleteOutline";
-import Edit from "@material-ui/icons/Edit";
-import FilterList from "@material-ui/icons/FilterList";
-import FirstPage from "@material-ui/icons/FirstPage";
-import LastPage from "@material-ui/icons/LastPage";
-import Remove from "@material-ui/icons/Remove";
-import SaveAlt from "@material-ui/icons/SaveAlt";
-import Search from "@material-ui/icons/Search";
-import ViewColumn from "@material-ui/icons/ViewColumn";
+import {
+  AddBox,
+  ArrowDownward,
+  Check,
+  ChevronLeft,
+  ChevronRight,
+  Clear,
+  DeleteOutline,
+  Edit,
+  FilterList,
+  FirstPage,
+  LastPage,
+  Remove,
+  SaveAlt,
+  Search,
+  ViewColumn
+} from "@material-ui/icons";
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  useMediaQuery,
+  useTheme,
+  Button,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Avatar,
+  Grid
+} from "@material-ui/core";
+import _ from "lodash";
 
 const tableIcons = {
   Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
@@ -50,9 +69,14 @@ export default function App() {
   const [fakeData, setFakeData] = useState(null);
   const [fData, setFData] = useState([]);
   const [selectedRow, setSelectedRow] = useState(null);
+  const [open, setOpen] = React.useState(false);
+  const [countryFD, setCountryFD] = React.useState(null);
+  const theme = useTheme();
+  const fullScreen = useMediaQuery(theme.breakpoints.down("xs"));
+
   const getFakeData = async () => {
     console.log("get api");
-    const res = await axios.get(`${api.url}?results=5`);
+    const res = await axios.get(`${api.url}?results=5000`);
     setFakeData(res.data.results);
   };
   useEffect(() => {
@@ -81,13 +105,30 @@ export default function App() {
     console.log("getFData");
     getFData(fakeData);
   }, [fakeData]);
-  console.warn("fakeData", fakeData);
-  console.warn("constData", fData);
-  const handleChangeCLick = (evt, selectedRow) => {
-    console.warn("selectRow", selectedRow);
-    setSelectedRow(selectedRow.tableData.id);
+  const getCountry = (fData) => {
+    let country = [];
+    fData && fData.map((c) => country.push(c.state));
+    _.mapKeys(country, function(value, key){return key + value} )
+    setCountryFD(_.uniq(country));
   };
-
+  useEffect(() => {
+    console.log("getCountry");
+    getCountry(fData);
+  }, [fData]);
+  console.warn("listCountry", JSON.stringify(countryFD));
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleChangeCLick = (evt, selectedRow) => {
+    setSelectedRow(selectedRow);
+    setOpen(true);
+  };
+  const getStatusRendering = (rowData) => {
+    return rowData.status === "female" ? <Check /> : <Remove />;
+  };
+  const getNoticeRendering = (rowData) => {
+    //return rowData.notice <= 5 ? <StarIcon /> : <StarHalfIcon />;
+  };
   return (
     <div style={{ maxWidth: "100%" }}>
       <MaterialTable
@@ -101,10 +142,18 @@ export default function App() {
             title: "Code postal",
             field: "postCode"
           },
-          { title: "Statut", field: "status" },
-          { title: "Etat", field: "state" },
-          { title: "Avis", field: "notice" },
-          { title: "Categorie", field: "category" }
+          {
+            title: "Statut",
+            field: "status",
+            render: (rowData) => getStatusRendering(rowData)
+          },
+          { title: "Etat", field: "state", lookup: { countryFD } },
+          {
+            title: "Avis",
+            field: "notice",
+            render: (rowData) => getNoticeRendering(rowData)
+          },
+          { title: "Categorie", field: "category", type: "numeric" }
         ]}
         data={fData}
         onRowClick={(evt, selectedRow) => handleChangeCLick(evt, selectedRow)}
@@ -112,9 +161,108 @@ export default function App() {
           rowStyle: (rowData) => ({
             backgroundColor:
               selectedRow === rowData.tableData.id ? "#EEE" : "#FFF"
-          })
+          }),
+          exportButton: true,
+          filtering: true
         }}
       />
+      {selectedRow && (
+        <Dialog
+          fullScreen={fullScreen}
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="responsive-dialog-title"
+        >
+          <DialogTitle id="responsive-dialog-title">
+            {"Info ligne selectionné"}
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              <Grid container spacing={1}>
+                <Grid item xs={10} md={4}>
+                  <div>
+                    <List dense>
+                      <ListItem>
+                        <ListItemAvatar>
+                          <Avatar>Id</Avatar>
+                        </ListItemAvatar>
+                        <ListItemText
+                          primary={`ìd de l'entreprise : ${selectedRow.id}`}
+                        />
+                      </ListItem>
+                      <ListItem>
+                        <ListItemAvatar>
+                          <Avatar>No</Avatar>
+                        </ListItemAvatar>
+                        <ListItemText
+                          primary={`nom de l'entreprise:  ${selectedRow.name}`}
+                        />
+                      </ListItem>
+                      <ListItem>
+                        <ListItemAvatar>
+                          <Avatar>Ad</Avatar>
+                        </ListItemAvatar>
+                        <ListItemText
+                          primary={`Adresse de l'entreprise: ${selectedRow.address}`}
+                        />
+                      </ListItem>
+                      <ListItem>
+                        <ListItemAvatar>
+                          <Avatar>Cp</Avatar>
+                        </ListItemAvatar>
+                        <ListItemText
+                          primary={`Code postal: ${selectedRow.postCode}`}
+                        />
+                      </ListItem>
+                    </List>
+                  </div>
+                </Grid>
+                <Grid item xs={10} md={4}>
+                  <div>
+                    <List dense>
+                      <ListItem>
+                        <ListItemAvatar>
+                          <Avatar>Vi</Avatar>
+                        </ListItemAvatar>
+                        <ListItemText primary={`Ville : ${selectedRow.city}`} />
+                      </ListItem>
+                      <ListItem>
+                        <ListItemAvatar>
+                          <Avatar>St</Avatar>
+                        </ListItemAvatar>
+                        <ListItemText
+                          primary={`Statut de l'entreprise: ${selectedRow.status}`}
+                        />
+                      </ListItem>
+                      <ListItem>
+                        <ListItemAvatar>
+                          <Avatar>Av</Avatar>
+                        </ListItemAvatar>
+                        <ListItemText
+                          primary={`Avis de l'entreprise: ${selectedRow.state}`}
+                        />
+                      </ListItem>
+                      <ListItem>
+                        <ListItemAvatar>
+                          <Avatar>Ct</Avatar>
+                        </ListItemAvatar>
+                        <ListItemText
+                          primary={`Catégorie de l'entreprise: ${selectedRow.category}`}
+                        />
+                      </ListItem>
+                    </List>
+                  </div>
+                </Grid>
+              </Grid>
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button autoFocus onClick={handleClose} color="primary">
+              Close
+            </Button>
+          </DialogActions>
+        </Dialog>
+      )}
     </div>
   );
 }
